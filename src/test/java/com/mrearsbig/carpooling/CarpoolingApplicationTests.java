ae83388f55f504f6aee024c958f38e121eb812f8
Alexander Viafara <mrearsbig@gmail.com>
2022-09-21 13:03:41 -0500

    Agregando la estructura del proyecto

---
+++ src/test/java/com/mrearsbig/carpooling/CarpoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrearsbig.carpooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
